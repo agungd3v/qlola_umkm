f0e5f1599a4a0bd35953420f1a5ae214245cc43e
Agung Ardiyanto <agungd3v@gmail.com>
2025-04-28 14:43:07 +0700

    rm: detault input sign in

--- lib/screens/auth/signin.dart
+++ lib/screens/auth/signin.dart
@@ -17,8 +17,8 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
-  final phone = TextEditingController(text: "81283134032");
-  final password = TextEditingController(text: "12345678");
+  final phone = TextEditingController();
+  final password = TextEditingController();
   bool showPassword = false;
   bool proccess = false;
 
